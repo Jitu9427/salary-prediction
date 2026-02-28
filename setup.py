c67b51f4eeb030c74493c3b290c0dad64c841927
jitu <jitu.prajapat0604@gmail.com>
2026-02-28 16:22:27 +0530

    project structure is ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="salary_prediction",
+    version="0.1.0",
+    author="Jitu prajapati",
+    description="MLOps pipeline for AI salary prediction",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
